d84da204062952519e9fe48dc051c527772306b6
zhanghengmao <zhanghengmao@sogou-inc.com>
2016-07-20 15:04:06 +0800

    modify

--- dashboard/init.php
+++ dashboard/init.php
@@ -1,7 +1,6 @@
 <?php
 include '..' . DIRECTORY_SEPARATOR . 'init.php';
 
-session_set_cookie_params(60*60*6);
 @session_start ();
 
 if (! isLogin ()) {
--- dashboard/login.php
+++ dashboard/login.php
@@ -2,12 +2,16 @@
 include '..' . DIRECTORY_SEPARATOR . 'init.php';
 include SROOT . 'userService.class.php';
 
+session_set_cookie_params(60*60*6);
+@session_start();
+
 if (v ( "username" ) && v ( "password" )) {
 	$service = new UserService ();
-	@session_start ();
 	if ($service->login ( v ( "username" ), v ( "password" ) )) {
 		header ( "Location: category_list.php" );
-	}
+    }else{
+        render('login');
+    }
 } else {
 	render ( "login" );
 }
